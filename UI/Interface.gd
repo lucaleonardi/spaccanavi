97675871e5a673e5e3852586f67a53eb76b6dba6
Luca Leonardi <lucaleonardi21@gmail.com>
2021-09-27 07:38:26 +0200

    project UI files clean up

--- UI/Interface.gd
+++ UI/Interface.gd
@@ -2,7 +2,6 @@ extends Control
 
 onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = $PauseOverlay
-onready var pause_title: Label = $PauseOverlay/Title
 onready var continue_btn: CenterContainer = $PauseOverlay/CenterContainer/Options/CenterContainer #/ContinueBtn
 onready var retry_btn: CenterContainer = $PauseOverlay/CenterContainer/Options/CenterContainer2 #/RetryBtn
 onready var restart_btn: CenterContainer = $PauseOverlay/CenterContainer/Options/CenterContainer3 #/RestartBtn
@@ -13,7 +12,6 @@ func _ready() -> void:
 	PlayerStats.connect("no_health", self, "_on_player_death")
 #
 func _on_player_death() -> void:
-	pause_title.text = "You died"
 	yield(get_tree().create_timer(1.5), "timeout")
 	pause_overlay.visible = true
 	continue_btn.visible = false
@@ -21,7 +19,7 @@ func _on_player_death() -> void:
 	retry_btn.visible = true
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause") and pause_title.text != "You died":
+	if event.is_action_pressed("pause") and get_tree().root.has_node("World/Player/Default"):
 		self.paused = not paused
 		scene_tree.set_input_as_handled()
 
